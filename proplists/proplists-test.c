206b7bb282e2aef8566de7a353f9c17a959ba2c2
Marek Majkowski <majek04@gmail.com>
2011-01-13 20:58:59 +0000

    support value size

--- proplists/proplists-test.c
+++ proplists/proplists-test.c
@@ -14,41 +14,42 @@ int main() {
         struct proplists_root root;
         INIT_PROPLISTS_ROOT(&root);
 
-        assert(proplists_find(&root, "a") == NULL);
-        assert(proplists_find(&root, "b") == NULL);
+        assert(proplists_find(&root, "a", NULL) == NULL);
+        assert(proplists_find(&root, "b", NULL) == NULL);
 
-        assert(proplists_add(&root, "c", "value_c") == 1);
-        assert(proplists_add(&root, "a", "value_a") == 1);
-        assert(proplists_add(&root, "b", "value_b") == 1);
-        assert(proplists_find(&root, "a") != NULL);
-        assert(proplists_find(&root, "b") != NULL);
-        assert(proplists_find(&root, "c") != NULL);
-        assert(strcmp(proplists_find(&root, "b"), "value_b") == 0);
-        assert(strcmp(proplists_find(&root, "a"), "value_a") == 0);
-        assert(strcmp(proplists_find(&root, "c"), "value_c") == 0);
-        assert(proplists_find(&root, "wrong") == NULL);
+        assert(proplists_add(&root, "c", "value_c", 0) == 1);
+        assert(proplists_add(&root, "a", "value_a", 0) == 1);
+        assert(proplists_add(&root, "b", "value_b", 0) == 1);
+        assert(proplists_find(&root, "a", NULL) != NULL);
+        assert(proplists_find(&root, "b", NULL) != NULL);
+        assert(proplists_find(&root, "c", NULL) != NULL);
+        assert(strcmp(proplists_find(&root, "b", NULL), "value_b") == 0);
+        assert(strcmp(proplists_find(&root, "a", NULL), "value_a") == 0);
+        assert(strcmp(proplists_find(&root, "c", NULL), "value_c") == 0);
+        assert(proplists_find(&root, "wrong", NULL) == NULL);
 
-        assert(proplists_add(&root, "a", "value_a2") == 0);
-        assert(strcmp(proplists_find(&root, "a"), "value_a") == 0);
+        assert(proplists_add(&root, "a", "value_a2", 0) == 0);
+        assert(strcmp(proplists_find(&root, "a", NULL), "value_a") == 0);
 
         struct list_head *pos;
         char *key, *value;
-        proplists_for_each(pos, &root, key, value) {
-                printf("%s -> %s\n", key, value);
+        int value_sz;
+        proplists_for_each(pos, &root, key, value, value_sz) {
+                printf("%s -> %s [%i]\n", key, value, value_sz);
         }
 
 
-        assert(proplists_find(&root, "c") != NULL);
+        assert(proplists_find(&root, "c", NULL) != NULL);
         assert(proplists_del(&root, "c") == 1);
-        assert(proplists_find(&root, "c") == NULL);
+        assert(proplists_find(&root, "c", NULL) == NULL);
 
-        assert(proplists_find(&root, "a") != NULL);
+        assert(proplists_find(&root, "a", NULL) != NULL);
         assert(proplists_del(&root, "a") == 1);
-        assert(proplists_find(&root, "a") == NULL);
+        assert(proplists_find(&root, "a", NULL) == NULL);
 
-        assert(proplists_find(&root, "b") != NULL);
+        assert(proplists_find(&root, "b", NULL) != NULL);
         assert(proplists_del(&root, "b") == 1);
-        assert(proplists_find(&root, "b") == NULL);
+        assert(proplists_find(&root, "b", NULL) == NULL);
 
 
         return 0;
--- proplists/proplists.h
+++ proplists/proplists.h
@@ -1,5 +1,14 @@
 #ifndef _PROPLISTS_H
 #define _PROPLISTS_H
+/* Proplists data structure: a list of key-value pairs.
+ *
+ * Assumptions:
+ *  - keys are relatively short null-terminated strings
+ *  - values are arbitrary length blobs
+ *  - this library always copies keys, user can free
+ *    the key just after usage
+ *  - value is managed by user, the library just copies the pointer
+ */
 
 /* You need:
 #include <stdint.h>
@@ -11,17 +20,23 @@
 struct proplists_item {
         struct list_head in_list;
         uint64_t key_hash;
-        char *value;
+        void *value;
+        int value_sz;
         char key[];
 };
 
 struct proplists_root {
+	void *(*mem_alloc)(int size);
+	void (*mem_free)(void *ptr, int size);
+
         struct list_head list_of_items;
 };
 
 
 //#define PROPLISTS_ROOT_INIT(name) (struct proplists_root) { {&(name).root,  &(name).root}}
 static inline void INIT_PROPLISTS_ROOT(struct proplists_root *root) {
+        root->mem_alloc = (void *(*)(int size))malloc;
+        root->mem_free = (void (*)(void *ptr, int size))free;
         INIT_LIST_HEAD(&root->list_of_items);
 }
 
@@ -41,13 +56,17 @@ static inline uint64_t  __proplists_hash(char *key, int *key_sz) {
 }
 
 
-static inline char *proplists_find(struct proplists_root *root, char *key) {
+static inline void *proplists_find(struct proplists_root *root,
+                                   char *key, int *value_sz) {
         uint64_t key_hash = __proplists_hash(key, NULL);
         struct list_head *head;
         list_for_each(head, &root->list_of_items) {
                 struct proplists_item *item = \
                         container_of(head, struct proplists_item, in_list);
                 if (item->key_hash == key_hash && strcmp(key, item->key) == 0) {
+                        if (value_sz) {
+                                *value_sz = item->value_sz;
+                        }
                         return item->value;
                 }
                 if (item->key_hash > key_hash) {
@@ -58,7 +77,7 @@ static inline char *proplists_find(struct proplists_root *root, char *key) {
 }
 
 static inline int proplists_add(struct proplists_root *root,
-                                char *key, char *value) {
+                                char *key, void *value, int value_sz) {
         int key_sz;
         uint64_t key_hash = __proplists_hash(key, &key_sz);
         struct list_head *head;
@@ -75,17 +94,16 @@ static inline int proplists_add(struct proplists_root *root,
         head = &root->list_of_items;
 
 insert:;
-        int value_sz = strlen(value);
+        int sz = sizeof(struct proplists_item) + key_sz + 1;
         struct proplists_item *new_item = (struct proplists_item*)      \
-                malloc(sizeof(struct proplists_item)
-                       + key_sz + 1 + value_sz + 1);
+                root->mem_alloc(sz);
         INIT_LIST_HEAD(&new_item->in_list);
 
         new_item->key_hash = key_hash;
         memcpy(new_item->key, key, key_sz + 1);
 
-        new_item->value = new_item->key + key_sz + 1;
-        memcpy(new_item->value, value, value_sz + 1);
+        new_item->value = value;
+        new_item->value_sz = value_sz;
 
         list_add_tail(&new_item->in_list, head);
         return 1;
@@ -100,7 +118,8 @@ static inline int proplists_del(struct proplists_root *root, char *key) {
                         container_of(head, struct proplists_item, in_list);
                 if (item->key_hash == key_hash && strcmp(key, item->key) == 0) {
                         list_del(&item->in_list);
-                        free(item);
+                        int sz = sizeof(struct proplists_item) + key_sz + 1;
+                        root->mem_free(item, sz);
                         return 1;
                 }
                 if (item->key_hash > key_hash) {
@@ -114,10 +133,11 @@ static inline int proplists_del(struct proplists_root *root, char *key) {
 
 
 /* based on list_for_each */
-#define proplists_for_each(pos, root, key, value)                       \
+#define proplists_for_each(pos, root, key, value, value_sz)             \
         for (pos = (root)->list_of_items.next; prefetch(pos->next),     \
                      (key) = container_of(pos, struct proplists_item, in_list)->key, \
                      (value) = container_of(pos, struct proplists_item, in_list)->value, \
+                     (value_sz) = container_of(pos, struct proplists_item, in_list)->value_sz, \
                      pos != &(root)->list_of_items;                    \
              pos = pos->next)
 
