8579bf1a2eb851f23fe9f5efdfe220664ec087e7
Marek Majkowski <majek04@gmail.com>
2011-02-01 12:55:39 +0000

    simple test for pika table encoding

---
+++ pika/test_table_encoding.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+'''
+Example of simple producer, creates one message and exits.
+'''
+import pika
+import sys
+import time
+
+
+if __name__ == '__main__':
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+            '127.0.0.1',
+            credentials=pika.PlainCredentials('guest', 'guest')))
+
+    channel = connection.channel()
+
+    r = channel.queue_declare(exclusive=True)
+
+    h = {
+            'a': 'a',
+            'b': 1,
+            'c': -1,
+            'd': 4611686018427387904L,
+            'e': -4611686018427387904L,
+            'f': [1,2,3],
+            'g': [1,2,3,[1,2,3]],
+            }
+
+    channel.basic_publish(exchange='',
+                          routing_key=r.queue,
+                          body='test',
+                          properties=pika.BasicProperties(headers=h))
+    channel.close()
+    channel = connection.channel()
+
+    a = channel.basic_get(queue=r.queue, no_ack=True)
+    print repr(h)
+    print repr(a._properties.headers)
+
+    
