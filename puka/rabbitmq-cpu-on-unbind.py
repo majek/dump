0d671c992df49eb93b5b6df050121f6c5a1285a9
Marek Majkowski <majek04@gmail.com>
2011-03-09 16:14:36 +0000

    unbind script

--- puka/rabbitmq-cpu-on-unbind.py
+++ puka/rabbitmq-cpu-on-unbind.py
@@ -5,9 +5,9 @@ sys.path.append("..")
 
 
 import puka
+import time
 
 
-print " [*] setting up"
 
 client = puka.Client("amqp://localhost/")
 
@@ -18,22 +18,51 @@ exchange = "test_"
 promise = client.exchange_declare(exchange=exchange, type="topic")
 client.wait(promise)
 
-promise = client.queue_declare(exclusive=True)
-queue = client.wait(promise)['queue']
 
-for i in xrange(10000):
-    promise = client.queue_bind(exchange=exchange,
-                                queue=queue,
-                                routing_key="blah.blah%i.*" % i)
+def create_queue(exchange, bindings):
+    promise = client.queue_declare(exclusive=True)
+    queue = client.wait(promise)['queue']
+
+    for i in xrange(bindings):
+        promise = client.queue_bind(exchange=exchange,
+                                    queue=queue,
+                                    routing_key="blah.blah%i.*" % i)
+        client.wait(promise)
+    return queue
+
+def queue_unbind(exchange, queue, bindings):
+    for i in xrange(bindings):
+        promise = client.queue_unbind(exchange=exchange,
+                                      queue=queue,
+                                      routing_key="blah.blah%i.*" % i)
+        client.wait(promise)
+    promise = client.queue_delete(queue)
     client.wait(promise)
 
-print " [*] done. deleting queue"
+def queue_delete(exchange, queue, bindings):
+    promise = client.queue_delete(queue)
+    client.wait(promise)
 
-import time
-t0 = time.time()
+for bindings in [100, 200, 1000, 1200]:
+    print "%i bindings -> " % bindings,
+    t0 = time.time()
+    queue = create_queue(exchange, bindings)
+    td = time.time() - t0
+    print " create=%.3fsec" % td,
 
-promise = client.queue_delete(queue)
-client.wait(promise)
-td = time.time() - t0
+    t0 = time.time()
+    queue = queue_delete(exchange, queue, bindings)
+    td = time.time() - t0
+    print " delete=%.3fsec" % td,
 
-print " [*] queue_delete took %f" % td
+    t0 = time.time()
+    queue = create_queue(exchange, bindings)
+    td = time.time() - t0
+    print " create=%.3fsec" % td,
+
+    t0 = time.time()
+    queue = queue_unbind(exchange, queue, bindings)
+    td = time.time() - t0
+    print " unbind=%.3fsec" % td,
+    print ""
+
