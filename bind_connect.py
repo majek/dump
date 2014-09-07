eb7fe4ef7096aad946fce2a12cdf192b52792fc5
Marek Majkowski <marek@cloudflare.com>
2014-09-07 22:10:28 +0100

    bind

--- bind_connect.py
+++ bind_connect.py
@@ -1,21 +1,68 @@
 import socket
 import random
 import time
+import errno
 
 
-HOSTS = ["google.com", "onet.pl", "wp.pl", "cloudflare.com"]
+a = []
+try:
+    for i in range(1000):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('192.168.97.139', 0))
+        a.append(s)
+except Exception, e:
+    print e
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.google.com", 80))
 
 
-port = random.randint(5000,6000)
+'''
+addr = ('192.168.97.139', 1091)
 
-print "port: %s" % (port,)
+print "port: %s" % (addr,)
+
+if True:
+    x = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    x.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    x.bind(addr)
+    x.connect(("idea.popcount.org", 80))
+
+
+for i in range(5):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(addr)
+    try:
+        s.connect(("idea.popcount.org", 80))
+        break
+    except socket.error, e:
+        if e.errno != errno.EADDRNOTAVAIL:
+            raise
+else:
+    raise Exception("Unable to allocate connection")
+
+print "waiting"
+time.sleep(130)
+'''
+
+
+'''
+HOSTS = ["idea.popcount.org", "www.wp.pl", "idea.popcount.org"]
+
+
+addr = ('192.168.97.139', 1099)  #random.randint(5000,6000)
+
+print "port: %s" % (addr,)
 sds = []
 for host in HOSTS:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('0.0.0.0', port))
+    #s.bind(('0.0.0.0', port))
+    s.bind(addr)
+    #s.setblocking(False)
     s.connect((host, 80))
-
     sds.append( s )
 
 for s in sds:
@@ -23,4 +70,6 @@ for s in sds:
     s.send("GET /")
 
 print "waiting"
-time.sleep(130)
+while True:
+    time.sleep(130)
+'''
