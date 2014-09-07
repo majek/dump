9216d9b460c6a885f3deca6284cd397adad7a67f
Marek Majkowski <marek@cloudflare.com>
2014-09-07 22:10:39 +0100

    nulldeeref

--- nullderef-module/nullderef.c
+++ nullderef-module/nullderef.c
@@ -18,6 +18,52 @@
 #include <linux/sched.h>
 #include <linux/hardirq.h>
 #include <linux/debugfs.h>
+#include <linux/export.h>
+#include <linux/moduleloader.h>
+#include <linux/ftrace_event.h>
+#include <linux/init.h>
+#include <linux/kallsyms.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/sysfs.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/elf.h>
+#include <linux/proc_fs.h>
+#include <linux/security.h>
+#include <linux/seq_file.h>
+#include <linux/syscalls.h>
+#include <linux/fcntl.h>
+#include <linux/rcupdate.h>
+#include <linux/capability.h>
+#include <linux/cpu.h>
+#include <linux/moduleparam.h>
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/vermagic.h>
+#include <linux/notifier.h>
+#include <linux/sched.h>
+#include <linux/stop_machine.h>
+#include <linux/device.h>
+#include <linux/string.h>
+#include <linux/mutex.h>
+#include <linux/rculist.h>
+#include <asm/uaccess.h>
+#include <asm/cacheflush.h>
+#include <asm/mmu_context.h>
+#include <linux/license.h>
+#include <asm/sections.h>
+#include <linux/tracepoint.h>
+#include <linux/ftrace.h>
+#include <linux/async.h>
+#include <linux/percpu.h>
+#include <linux/kmemleak.h>
+#include <linux/jump_label.h>
+#include <linux/pfn.h>
+#include <linux/bsearch.h>
+#include <linux/fips.h>
+#include <uapi/linux/module.h>
 
 /*
  * Define an 'ops' struct containing a single mostly-pointless
@@ -92,6 +138,12 @@ static void cleanup_debugfs(void) {
  */
 static int __init nullderef_init(void)
 {
+
+	printk(KERN_INFO "info\n");
+	printk(KERN_NOTICE "notice\n");
+	printk(KERN_DEBUG "debug\n");
+	printk(KERN_WARNING "warn\n");
+
 	/* Create the directory our files will live in. */
 	nullderef_root = debugfs_create_dir("nullderef", NULL);
 	if (!nullderef_root) {
@@ -112,6 +164,29 @@ static int __init nullderef_init(void)
 
 	if (!read_de || !call_de)
 		goto out_err;
+
+
+	/* void *addr = suicide; */
+	/* long size, offset; */
+	/* char *modname; */
+	/* char namebuf[128]; */
+
+	//module_address_lookup((long)addr, &size, &offset, &modname, namebuf);
+
+	//printk(KERN_DEFAULT "%s 0x%lx/0x%lx [%s]\n", namebuf, offset, size, modname);
+
+	printk(KERN_DEFAULT "%pB\n", printk);
+	printk(KERN_DEFAULT "%pB\n", (void*)((long)printk + 1));
+
+	unsigned long a = kallsyms_lookup_name("printk");
+	unsigned long b = printk;
+
+	printk(KERN_DEFAULT "x: %pB, 0x%lx, 0x%lx\n", printk, a, b);
+
+	char buf[128];
+	sprint_symbol_no_offset(buf, (long)printk);
+	printk(KERN_DEFAULT "x: %pB, 0x%lx, 0x%lx %s\n", printk, a, b, buf);
+
 
 	return 0;
 out_err:
@@ -134,6 +209,122 @@ static void __exit nullderef_exit(void)
  */
 module_init(nullderef_init);
 module_exit(nullderef_exit);
+
+
+
+
+
+
+
+#if 0
+
+static inline int is_arm_mapping_symbol(const char *str)
+{
+	return str[0] == '$' && strchr("atd", str[1])
+	       && (str[2] == '\0' || str[2] == '.');
+}
+
+static const char *get_ksymbol(struct module *mod,
+			       unsigned long addr,
+			       unsigned long *size,
+			       unsigned long *offset)
+{
+	unsigned int i, best = 0;
+	unsigned long nextval;
+
+	/* At worse, next value is at end of module */
+	if (within_module_init(addr, mod))
+		nextval = (unsigned long)mod->module_init+mod->init_text_size;
+	else
+		nextval = (unsigned long)mod->module_core+mod->core_text_size;
+
+	/* Scan for closest preceding symbol, and next symbol. (ELF
+	   starts real symbols at 1). */
+	for (i = 1; i < mod->num_symtab; i++) {
+		if (mod->symtab[i].st_shndx == SHN_UNDEF)
+			continue;
+
+		/* We ignore unnamed symbols: they're uninformative
+		 * and inserted at a whim. */
+		if (mod->symtab[i].st_value <= addr
+		    && mod->symtab[i].st_value > mod->symtab[best].st_value
+		    && *(mod->strtab + mod->symtab[i].st_name) != '\0'
+		    && !is_arm_mapping_symbol(mod->strtab + mod->symtab[i].st_name))
+			best = i;
+		if (mod->symtab[i].st_value > addr
+		    && mod->symtab[i].st_value < nextval
+		    && *(mod->strtab + mod->symtab[i].st_name) != '\0'
+		    && !is_arm_mapping_symbol(mod->strtab + mod->symtab[i].st_name))
+			nextval = mod->symtab[i].st_value;
+	}
+
+	if (!best)
+		return NULL;
+
+	if (size)
+		*size = nextval - mod->symtab[best].st_value;
+	if (offset)
+		*offset = addr - mod->symtab[best].st_value;
+	return mod->strtab + mod->symtab[best].st_name;
+}
+
+/* For kallsyms to ask for address resolution.  NULL means not found.  Careful
+ * not to lock to avoid deadlock on oopses, simply disable preemption. */
+const char *module_address_lookup(unsigned long addr,
+			    unsigned long *size,
+			    unsigned long *offset,
+			    char **modname,
+			    char *namebuf)
+{
+	struct module *mod;
+	const char *ret = NULL;
+
+	preempt_disable();
+	list_for_each_entry_rcu(mod, &modules, list) {
+		if (mod->state == MODULE_STATE_UNFORMED)
+			continue;
+		if (within_module_init(addr, mod) ||
+		    within_module_core(addr, mod)) {
+			if (modname)
+				*modname = mod->name;
+			ret = get_ksymbol(mod, addr, size, offset);
+			break;
+		}
+	}
+	/* Make a copy in here where it's safe */
+	if (ret) {
+		strncpy(namebuf, ret, KSYM_NAME_LEN - 1);
+		ret = namebuf;
+	}
+	preempt_enable();
+	return ret;
+}
+
+int lookup_module_symbol_name(unsigned long addr, char *symname)
+{
+	struct module *mod;
+
+	preempt_disable();
+	list_for_each_entry_rcu(mod, &modules, list) {
+		if (mod->state == MODULE_STATE_UNFORMED)
+			continue;
+		if (within_module_init(addr, mod) ||
+		    within_module_core(addr, mod)) {
+			const char *sym;
+
+			sym = get_ksymbol(mod, addr, NULL, NULL);
+			if (!sym)
+				goto out;
+			strlcpy(symname, sym, KSYM_NAME_LEN);
+			preempt_enable();
+			return 0;
+		}
+	}
+out:
+	preempt_enable();
+	return -ERANGE;
+}
+#endif
 
 MODULE_AUTHOR("Nelson Elhage <nelhage@ksplice.com>");
 MODULE_DESCRIPTION("Provides debugfs files to trigger NULL pointer dereferences.");
